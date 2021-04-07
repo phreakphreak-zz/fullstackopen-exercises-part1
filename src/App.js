1c6c9f5a5e041c42ab27da9e1cdce35967e2d0eb
Michael Zabala (Mike) <wonkaphreak@gmail.com>
2021-04-07 17:01:59 -0400

    exercises 1.12*

--- src/App.js
+++ src/App.js
@@ -1,125 +1,31 @@
 import React, { useState } from "react";
 
-const Button = ({ stat }) => {
-  const { handleClick, text } = stat;
-  return <button onClick={handleClick}>{text}</button>;
+const Button = ({ handlerClick }) => {
+  return <button onClick={handlerClick}>Next Phrase</button>;
 };
-const Buttons = ({ feedback }) => {
-  const [good, neutral, bad] = feedback.statesFeedback;
-  return (
-    <div>
-      <Button stat={good} />
-      <Button stat={neutral} />
-      <Button stat={bad} />
-    </div>
-  );
-};
-
-const Statistic = ({ stat }) => {
-  const { text, value } = stat;
-  let paragraph = text ==="positive"?`${text}: ${value}%`:`${text}: ${value}`
-  return (
-    <p>
-      {paragraph}
-    </p>
-  );
-};
-const Statistics = ({ feedback }) => {
-  const [good, neutral, bad, all, average, positive] = feedback.statesFeedback; 
-  if (all.value > 0) {
-    return (
-      <div>
-        <h1>{feedback.titleStatistics}</h1>
-        <Statistic stat={good} /> 
-        <Statistic stat={neutral} />
-        <Statistic stat={bad} />
-        <Statistic stat={all} />
-        <Statistic stat={average} />
-        <Statistic stat={positive} />
-      </div>
-    ); 
-  } else {
-    return (
-      <div>
-        <h1>{feedback.titleStatistics}</h1>
-        <p>no given feedback</p>
-      </div>
-    );
-  }
-};
-
-const Header = ({ feedback }) => <h1>{feedback.titleHeader}</h1>;
 
 const App = () => {
-  const [stats, setStats] = useState({
-    all: 0,
-    average: 0,
-    positive: 0,
-  });
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const anecdotes = [
+    "If it hurts, do it more often",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+  ];
 
-  const feedback = {
-    titleHeader: "Give Feedback",
-    titleStatistics: "Statistics",
-    statesFeedback: [
-      {
-        text: "good",
-        value: good,
-        handleClick: () => {
-          setStats({
-            all: stats.all + 1,
-            average: (stats.all + 1) / 3,
-            positive:(100*(good+1))/(stats.all+1)
-          });
-          setGood(good + 1);
-        },
-      },
-      {
-        text: "neutral",
-        value: neutral,
-        handleClick: () => {
-          setStats({
-            all: stats.all + 1,
-            average: (stats.all + 1) / 3,
-            positive:(100*(good))/(stats.all+1)
-          });
-          setNeutral(neutral + 1);
-        },
-      },
-      {
-        text: "bad",
-        value: bad,
-        handleClick: () => {
-          setStats({
-            all: stats.all + 1,
-            average: (stats.all + 1) / 3,
-            positive:(100*(good))/(stats.all+1)
-          });
-          setBad(bad + 1);
-        },
-      },
-      {
-        text: "all",
-        value: stats.all,
-      },
-      {
-        text: "average",
-        value: stats.average,
-      },
-      {
-        text: "positive",
-        value: stats.positive.toFixed(1),
-      },
-    ],
+  const [selected, setSelected] = useState(0);
+
+  const handlerClick = () => {
+    let element = Math.round(Math.random() * (anecdotes.length - 1));
+    console.log(element);
+    return setSelected(element.toFixed(0));
   };
-
   return (
     <div>
-      <Header feedback={feedback} />
-      <Buttons feedback={feedback} />
-      <Statistics feedback={feedback} />
+      {anecdotes[selected]}
+      <br />
+      <Button handlerClick={handlerClick} />
     </div>
   );
 };
